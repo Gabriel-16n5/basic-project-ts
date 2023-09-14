6e5f2ae8e2f30d2b6080a39062f3b61dd7c74ea3
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-09-14 17:04:43 -0300

    fix: permitir o uso de ssl para conexão de banco de dados

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,9 +6,12 @@ const { Pool } = pg
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
+  ssl: false
 };
 
 const connection = new Pool(configDatabase);
+
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 export default connection
 
